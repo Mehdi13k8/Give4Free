00846c52b48d63af071ef111187187bac981b8c9
mehdi rhoulam <mehdi.rhoulam@epitech.eu>
2020-06-13 11:30:54 +0200

    init

---
+++ src/main/java/com/example/Give4Free/Give4FreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Give4Free;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Give4FreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Give4FreeApplication.class, args);
+	}
+
+}
